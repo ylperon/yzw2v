7ccb85ab5b1ea62c6458b146990fbf7d70ed16cf
Kostya Bazhanov <kostyabazhanov@mail.ru>
2016-03-21 00:23:10 +0300

    oops

--- mem.cpp
+++ mem.cpp
@@ -1,5 +1,7 @@
 #if defined(__linux__) || defined(__APPLE__)
 #include "mem_posix.cpp"
+#elif defined(_WIN32) || defined(_WIN64)
+#include "mem_win.cpp"
 #else
 #error "No implementation for current platform"
 #endif
