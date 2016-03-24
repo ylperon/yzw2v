485433cc7104e260519407a88aa093b24f66a8f0
Kostya Bazhanov <kostyabazhanov@mail.ru>
2016-03-25 00:08:46 +0300

    drop prefetch

--- numeric_avx.cpp
+++ numeric_avx.cpp
@@ -6,7 +6,6 @@ void yzw2v::num::Fill(float* v, const uint32_t v_size, const float value) noexce
     const auto* const v_end = v + v_size;
     const auto wide_value = _mm256_set1_ps(value);
     for (const auto* const v_this_end = v_end - 7; v < v_this_end; v += 8) {
-        _mm_prefetch(v + 8, _MM_HINT_T0);
         _mm256_store_ps(v, wide_value);
     }
 
@@ -23,7 +22,6 @@ void yzw2v::num::DivideVector(float* v, const uint32_t v_size, const float divis
     const auto* const v_end = v + v_size;
     const auto wide_divisor = _mm256_set1_ps(divisor);
     for (const auto* const v_this_end = v_end - 7; v < v_this_end; v += 8) {
-        _mm_prefetch(v + 8, _MM_HINT_T0);
         _mm256_store_ps(v, _mm256_div_ps(_mm256_load_ps(v), wide_divisor));
     }
 
@@ -36,7 +34,6 @@ void yzw2v::num::MultiplyVector(float* v, const uint32_t v_size, const float mul
     const auto* const v_end = v + v_size;
     const auto wide_multiple = _mm256_set1_ps(multiple);
     for (const auto* const v_this_end = v_end - 7; v < v_this_end; v += 8) {
-        _mm_prefetch(v + 8, _MM_HINT_T0);
         _mm256_store_ps(v, _mm256_mul_ps(_mm256_load_ps(v), wide_multiple));
     }
 
@@ -48,8 +45,6 @@ void yzw2v::num::MultiplyVector(float* v, const uint32_t v_size, const float mul
 void yzw2v::num::AddVector(float* v, const uint32_t v_size, const float* summand) noexcept {
     const auto* const v_end = v + v_size;
     for (const auto* const v_this_end = v_end - 7; v < v_this_end; summand += 8, v += 8) {
-        _mm_prefetch(v + 8, _MM_HINT_T0);
-        _mm_prefetch(summand + 8, _MM_HINT_T0);
         _mm256_store_ps(v, _mm256_add_ps(_mm256_load_ps(v), _mm256_load_ps(summand)));
     }
 
@@ -63,8 +58,6 @@ void yzw2v::num::AddVector(float* v, const uint32_t v_size,
     const auto* const v_end = v + v_size;
     const auto wide_summand_multiple = _mm256_set1_ps(summand_multiple);
     for (const auto* const v_this_end = v_end - 7; v < v_this_end; summand += 8, v += 8) {
-        _mm_prefetch(v + 8, _MM_HINT_T0);
-        _mm_prefetch(summand + 8, _MM_HINT_T0);
         _mm256_store_ps(v, _mm256_add_ps(_mm256_load_ps(v),
                                          _mm256_mul_ps(wide_summand_multiple,
                                                        _mm256_load_ps(summand)
@@ -83,8 +76,6 @@ float yzw2v::num::ScalarProduct(const float* v, const uint32_t v_size,
     __m256 wide_res = {};
     const auto* const v_end = v + v_size;
     for (const auto* const v_this_end = v_end - 7; v < v_this_end; v += 8, rhs += 8) {
-        _mm_prefetch(v + 8, _MM_HINT_T0);
-        _mm_prefetch(rhs + 8, _MM_HINT_T0);
         wide_res = _mm256_add_ps(wide_res, _mm256_mul_ps(_mm256_load_ps(v), _mm256_load_ps(rhs)));
     }
 
