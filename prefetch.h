1bc8e8f0906d47cb0610e9e460f61af39c76479b
Kostya Bazhanov <kostyabazhanov@mail.ru>
2016-03-26 17:06:34 +0300

    no need for explicit cast

--- prefetch.h
+++ prefetch.h
@@ -1,8 +1,8 @@
 #pragma once
 
 #if defined(__GNUC__)
-#define YZ_PREFETCH_READ(ptr, priority) __builtin_prefetch(reinterpret_cast<const void*>(ptr), 0, priority)
-#define YZ_PREFETCH_WRITE(ptr, priority) __builtin_prefetch(reinterpret_cast<const void*>(ptr), 1, priority)
+#define YZ_PREFETCH_READ(ptr, priority) __builtin_prefetch(ptr, 0, priority)
+#define YZ_PREFETCH_WRITE(ptr, priority) __builtin_prefetch(ptr, 1, priority)
 #else
 #define YZ_PREFETCH_READ(ptr, priority) (void)(Pointer), (void)Priority
 #define YZ_PREFETCH_WRITE(ptr, priority) (void)(Pointer), (void)Priority
