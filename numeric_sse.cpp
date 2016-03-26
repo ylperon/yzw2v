0836070e07cd60a48fdc3f63853b4f84ddb2b27d
Kostya Bazhanov <kostyabazhanov@mail.ru>
2016-03-26 17:26:36 +0300

    yep, I do

--- numeric_sse.cpp
+++ numeric_sse.cpp
@@ -6,7 +6,7 @@
 #include <xmmintrin.h>
 
 void yzw2v::num::Fill(float* v, const uint32_t v_size, const float value) noexcept {
-    v = YZ_ASSUME_ALIGNED(v, 32);
+    v = YZ_ASSUME_ALIGNED(v, 128);
     const auto wide_value = _mm_set1_ps(value);
     for (const auto* const v_end = v + mem::RoundSizeUpByVecSize(v_size); v < v_end; v += 4) {
         _mm_store_ps(v, wide_value);
@@ -18,7 +18,7 @@ void yzw2v::num::Zeroize(float* const v, const uint32_t v_size) noexcept {
 }
 
 void yzw2v::num::DivideVector(float* v, const uint32_t v_size, const float divisor) noexcept {
-    v = YZ_ASSUME_ALIGNED(v, 32);
+    v = YZ_ASSUME_ALIGNED(v, 128);
     const auto wide_divisor = _mm_set1_ps(divisor);
     for (const auto* const v_end = v + mem::RoundSizeUpByVecSize(v_size); v < v_end; v += 4) {
         _mm_store_ps(v, _mm_div_ps(_mm_load_ps(v), wide_divisor));
@@ -26,7 +26,7 @@ void yzw2v::num::DivideVector(float* v, const uint32_t v_size, const float divis
 }
 
 void yzw2v::num::MultiplyVector(float* v, const uint32_t v_size, const float multiple) noexcept {
-    v = YZ_ASSUME_ALIGNED(v, 32);
+    v = YZ_ASSUME_ALIGNED(v, 128);
     const auto wide_multiple = _mm_set1_ps(multiple);
     for (const auto* const v_end = v + mem::RoundSizeUpByVecSize(v_size); v < v_end; v += 4) {
         _mm_store_ps(v, _mm_mul_ps(_mm_load_ps(v), wide_multiple));
@@ -34,8 +34,8 @@ void yzw2v::num::MultiplyVector(float* v, const uint32_t v_size, const float mul
 }
 
 void yzw2v::num::AddVector(float* v, const uint32_t v_size, const float* summand) noexcept {
-    v = YZ_ASSUME_ALIGNED(v, 32);
-    summand = YZ_ASSUME_ALIGNED(summand, 32);
+    v = YZ_ASSUME_ALIGNED(v, 128);
+    summand = YZ_ASSUME_ALIGNED(summand, 128);
     for (const auto* const v_end = v + mem::RoundSizeUpByVecSize(v_size); v < v_end; summand += 4, v += 4) {
         _mm_store_ps(v, _mm_add_ps(_mm_load_ps(v), _mm_load_ps(summand)));
     }
@@ -43,8 +43,8 @@ void yzw2v::num::AddVector(float* v, const uint32_t v_size, const float* summand
 
 void yzw2v::num::AddVector(float* v, const uint32_t v_size,
                            const float* summand, const float summand_multiple) noexcept {
-    v = YZ_ASSUME_ALIGNED(v, 32);
-    summand = YZ_ASSUME_ALIGNED(summand, 32);
+    v = YZ_ASSUME_ALIGNED(v, 128);
+    summand = YZ_ASSUME_ALIGNED(summand, 128);
     const auto wide_summand_multiple = _mm_set1_ps(summand_multiple);
     for (const auto* const v_end = v + mem::RoundSizeUpByVecSize(v_size); v < v_end; summand += 4, v += 4) {
         _mm_store_ps(v, _mm_add_ps(_mm_load_ps(v),
@@ -58,8 +58,8 @@ void yzw2v::num::AddVector(float* v, const uint32_t v_size,
 
 float yzw2v::num::ScalarProduct(const float* v, const uint32_t v_size,
                                 const float* rhs) noexcept {
-    v = YZ_ASSUME_ALIGNED(v, 32);
-    rhs = YZ_ASSUME_ALIGNED(rhs, 32);
+    v = YZ_ASSUME_ALIGNED(v, 128);
+    rhs = YZ_ASSUME_ALIGNED(rhs, 128);
     __m128 wide_res = {};
     for (const auto* const v_end = v + mem::RoundSizeUpByVecSize(v_size); v < v_end; v += 4, rhs += 4) {
         wide_res = _mm_add_ps(wide_res, _mm_mul_ps(_mm_load_ps(v), _mm_load_ps(rhs)));
