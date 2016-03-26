dd70c888ae2af426fdf8a9f0d02b4ab99764c098
Kostya Bazhanov <kostyabazhanov@mail.ru>
2016-03-27 01:06:09 +0300

    doh

--- prefetch.h
+++ prefetch.h
@@ -4,6 +4,6 @@
 #define YZ_PREFETCH_READ(ptr, priority) __builtin_prefetch(ptr, 0, priority)
 #define YZ_PREFETCH_WRITE(ptr, priority) __builtin_prefetch(ptr, 1, priority)
 #else
-#define YZ_PREFETCH_READ(ptr, priority) (void)(pointer), (void)priority
-#define YZ_PREFETCH_WRITE(ptr, priority) (void)(pointer), (void)priority
+#define YZ_PREFETCH_READ(ptr, priority) (void)(ptr), (void)priority
+#define YZ_PREFETCH_WRITE(ptr, priority) (void)(ptr), (void)priority
 #endif
