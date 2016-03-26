126e4ecc00e1168a3198def7b211efd42596b636
Kostya Bazhanov <kostyabazhanov@mail.ru>
2016-03-27 01:03:36 +0300

    like this?

--- numeric.cpp
+++ numeric.cpp
@@ -1,10 +1,10 @@
 #if defined(__AVX__)
-#pragma message "[yzw2v] will use AVX instruction set for linear algebra"
+#pragma message("[yzw2v] will use AVX instruction set for linear algebra")
 #include "numeric_avx.cpp"
 #elif defined(__SSE__)
-#pragma message "[yzw2v] will use SSE instruction set for linear algebra"
+#pragma message("[yzw2v] will use SSE instruction set for linear algebra")
 #include "numeric_sse.cpp"
 #else
-#pragma message "[yzw2v] will use basic C++ code for linear algebra"
+#pragma message("[yzw2v] will use basic C++ code for linear algebra")
 #include "numeric_simple.cpp"
 #endif
