71f7032a3d313b7c142481d353cdc3537d168003
Kostya Bazhanov <kostyabazhanov@mail.ru>
2016-04-01 21:25:33 +0300

    trying to simplify profiling

--- numeric_sse.cpp
+++ numeric_sse.cpp
@@ -21,7 +21,9 @@ void yzw2v::num::DivideVector(float* v, const uint32_t v_size, const float divis
     v = YZ_ASSUME_ALIGNED(v, 128);
     const auto wide_divisor = _mm_set1_ps(divisor);
     for (const auto* const v_end = v + mem::RoundSizeUpByVecSize(v_size); v < v_end; v += 4) {
-        _mm_store_ps(v, _mm_div_ps(_mm_load_ps(v), wide_divisor));
+        const auto x = _mm_load_ps(v);
+        const auto y = _mm_div_ps(x, wide_divisor);
+        _mm_store_ps(v, y);
     }
 }
 
@@ -29,7 +31,9 @@ void yzw2v::num::MultiplyVector(float* v, const uint32_t v_size, const float mul
     v = YZ_ASSUME_ALIGNED(v, 128);
     const auto wide_multiple = _mm_set1_ps(multiple);
     for (const auto* const v_end = v + mem::RoundSizeUpByVecSize(v_size); v < v_end; v += 4) {
-        _mm_store_ps(v, _mm_mul_ps(_mm_load_ps(v), wide_multiple));
+        const auto x = _mm_load_ps(v);
+        const auto y = _mm_mul_ps(x, wide_multiple);
+        _mm_store_ps(v, y);
     }
 }
 
@@ -37,7 +41,10 @@ void yzw2v::num::AddVector(float* v, const uint32_t v_size, const float* summand
     v = YZ_ASSUME_ALIGNED(v, 128);
     summand = YZ_ASSUME_ALIGNED(summand, 128);
     for (const auto* const v_end = v + mem::RoundSizeUpByVecSize(v_size); v < v_end; summand += 4, v += 4) {
-        _mm_store_ps(v, _mm_add_ps(_mm_load_ps(v), _mm_load_ps(summand)));
+        const auto x = _mm_load_ps(v);
+        const auto y = _mm_load_ps(summand);
+        const auto z = _mm_add_ps(x, y);
+        _mm_store_ps(v, z);
     }
 }
 
@@ -47,12 +54,11 @@ void yzw2v::num::AddVector(float* v, const uint32_t v_size,
     summand = YZ_ASSUME_ALIGNED(summand, 128);
     const auto wide_summand_multiple = _mm_set1_ps(summand_multiple);
     for (const auto* const v_end = v + mem::RoundSizeUpByVecSize(v_size); v < v_end; summand += 4, v += 4) {
-        _mm_store_ps(v, _mm_add_ps(_mm_load_ps(v),
-                                   _mm_mul_ps(wide_summand_multiple,
-                                              _mm_load_ps(summand)
-                                              )
-                                   )
-                     );
+        const auto x = _mm_load_ps(summand);
+        const auto y = _mm_load_ps(v);
+        const auto z = _mm_mul_ps(x, wide_summand_multiple);
+        const auto w = _mm_add_ps(y, z);
+        _mm_store_ps(v, w);
     }
 }
 
@@ -62,7 +68,10 @@ float yzw2v::num::ScalarProduct(const float* v, const uint32_t v_size,
     rhs = YZ_ASSUME_ALIGNED(rhs, 128);
     __m128 wide_res = {};
     for (const auto* const v_end = v + mem::RoundSizeUpByVecSize(v_size); v < v_end; v += 4, rhs += 4) {
-        wide_res = _mm_add_ps(wide_res, _mm_mul_ps(_mm_load_ps(v), _mm_load_ps(rhs)));
+        const auto x = _mm_load_ps(v);
+        const auto y = _mm_load_ps(rhs);
+        const auto z = _mm_mul_ps(x, y);
+        wide_res = _mm_add_ps(wide_res, z);
     }
 
     return wide_res[0] + wide_res[1] + wide_res[2] + wide_res[3];
