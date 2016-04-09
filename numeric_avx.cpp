ea3e5357f9cfab081c66a8efdaba4a7bb47fe47d
Kostya Bazhanov <kostyabazhanov@mail.ru>
2016-04-09 18:02:30 +0300

    unroll scalar product for AVX too

--- numeric_avx.cpp
+++ numeric_avx.cpp
@@ -66,13 +66,38 @@ void yzw2v::num::AddVector(float* v, const uint32_t v_size,
 
 float yzw2v::num::ScalarProduct(const float* v, const uint32_t v_size,
                                 const float* rhs) noexcept {
+    const auto* const v_end_rounded_up = v + mem::RoundSizeUpByVecSize(v_size);
     v = YZ_ASSUME_ALIGNED(v, 256);
     rhs = YZ_ASSUME_ALIGNED(rhs, 256);
-    __m256 wide_res = {};
-    for (const auto* const v_end = v + mem::RoundSizeUpByVecSize(v_size); v < v_end; v += 8, rhs += 8) {
-        wide_res = _mm256_add_ps(wide_res, _mm256_mul_ps(_mm256_load_ps(v), _mm256_load_ps(rhs)));
+
+    __m256 wide_res[8] = {};
+    for (const auto* const v_end = v + ((v_end_rounded_up - v) % 64); v < v_end; v += 8, rhs += 8) {
+        wide_res[0] = _mm256_add_ps(wide_res[0], _mm256_mul_ps(_mm256_load_ps(v), _mm256_load_ps(rhs)));
     }
 
-    return wide_res[0] + wide_res[1] + wide_res[2] + wide_res[3]
-           + wide_res[4] + wide_res[5] + wide_res[6] + wide_res[7];
+    // maybe we should unroll for 32 float between?
+
+    for (; v < v_end_rounded_up; v += 64, rhs += 64) {
+        wide_res[0] = _mm256_add_ps(wide_res[0], _mm256_mul_ps(_mm256_load_ps(v), _mm256_load_ps(rhs)));
+        wide_res[1] = _mm256_add_ps(wide_res[1], _mm256_mul_ps(_mm256_load_ps(v + 8), _mm256_load_ps(rhs + 8)));
+        wide_res[2] = _mm256_add_ps(wide_res[2], _mm256_mul_ps(_mm256_load_ps(v + 16), _mm256_load_ps(rhs + 16)));
+        wide_res[3] = _mm256_add_ps(wide_res[3], _mm256_mul_ps(_mm256_load_ps(v + 24), _mm256_load_ps(rhs + 24)));
+        wide_res[4] = _mm256_add_ps(wide_res[4], _mm256_mul_ps(_mm256_load_ps(v + 32), _mm256_load_ps(rhs + 32)));
+        wide_res[5] = _mm256_add_ps(wide_res[5], _mm256_mul_ps(_mm256_load_ps(v + 40), _mm256_load_ps(rhs + 40)));
+        wide_res[6] = _mm256_add_ps(wide_res[6], _mm256_mul_ps(_mm256_load_ps(v + 48), _mm256_load_ps(rhs + 48)));
+        wide_res[7] = _mm256_add_ps(wide_res[7], _mm256_mul_ps(_mm256_load_ps(v + 56), _mm256_load_ps(rhs + 56)));
+    }
+
+    wide_res[0] = _mm256_add_ps(wide_res[0], wide_res[1]);
+    wide_res[2] = _mm256_add_ps(wide_res[2], wide_res[3]);
+    wide_res[4] = _mm256_add_ps(wide_res[4], wide_res[5]);
+    wide_res[6] = _mm256_add_ps(wide_res[6], wide_res[7]);
+
+    wide_res[0] = _mm256_add_ps(wide_res[0], wide_res[2]);
+    wide_res[4] = _mm256_add_ps(wide_res[4], wide_res[6]);
+
+    wide_res[0] = _mm256_add_ps(wide_res[0], wide_res[4]);
+
+    return wide_res[0][0] + wide_res[0][1] + wide_res[0][2] + wide_res[0][3]
+           + wide_res[0][4] + wide_res[0][5] + wide_res[0][6] + wide_res[0][7];
 }
